ace09aa5162c8cfda4a25415e5aac5b4386ac66c
andrew <andrewacalrk248@gmail.com>
2022-01-29 16:38:05 -0500

    added test that does not work

--- test.py
+++ test.py
@@ -2,16 +2,26 @@ import unittest
 import basicpytonscript
 
 
-class TestSum(unittest.TestCase):
+class TestScript(unittest.TestCase):
 
 
-	def returns_with_success(self):
+	def test_returns_with_success_1(self):
 		result = basicpytonscript.generate_final_output()
 		
 		number_of_unique_products = result["number_of_unique_products"]
 		number_of_unique_brands = result["number_of_unique_brands"]
 		avg_price = result["avg_price"]
 
-		self.assertEqual(number_of_unique_products, 1)
-		#self.assertIsNot(number_of_unique_brands, None)
-		#self.assertIsNot(avg_price, None)
+		self.assertIsNot(number_of_unique_products, None)
+		self.assertIsNot(number_of_unique_brands, None)
+		self.assertIsNot(avg_price, None)
+
+
+	def test_correct_number_of_unique_products(self):
+		result = basicpytonscript.generate_final_output()
+		
+		number_of_unique_products = result["number_of_unique_products"]
+		products = basicpytonscript.clean_data()
+		actual_number_of_unique_products = len(products)
+
+		self.assertEqual(number_of_unique_products, actual_number_of_unique_products)
